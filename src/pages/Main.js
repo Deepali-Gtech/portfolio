1dd3221470cbafb9df092a8b697d07e9924bc752
Deepali Garg <deepaligarg.chd@gmail.com>
2021-06-10 19:15:53 -0400

    added basic styling to bio page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,17 +5,21 @@ function Main() {
     return (
         // eslint-disable-next-line react/style-prop-object
         <main class="container">
-            <div style={{ backgroundImage: `url("./software_img.jpg")`, backgroundRepeat: "repeat-y", backgroundSize: "100%" }}>
-                <h1><b>Designer & Coder</b></h1>
+            <div id="full" style={{ backgroundImage: `url("./software_img.jpg")`, backgroundRepeat: "repeat-y", backgroundSize: "100%" }}>
+            <h2><b>Deepali Garg</b></h2>
                 <div id="profileImage">
-                    <img src="./deepali.png" alt="Remote Classroom" class="img-fluid col-sm-4 p-2" />
+                    
+                    <img src="./deepali.png" alt="Remote Classroom" class="img-fluid col-sm-8 p-2" />
                 </div>
-                <h2><b>Deepali Garg</b></h2>
-                <p>Web Developer</p><br />
+
+                <div id="bio">
+                    <p>Full-stack Web Developer. Innovation optimized solution seeker. Recently earned a certificate in full stack development from The Georgia Institute of Technology, with newly developed skills in JavaScript, CSS, React.js, Node, Express, Mongodb, SQL and responsive web design. Innovative problem solver passionate about developing apps, with a focus on mobile-first design and development. Also worked on a team of four to develop a single-page MERN app that helps people to connect through fitness activities. I am an effective communicator and team player, demonstrated throughout my bootcamp, where I kept open and regular communication with my teammates. Exhibited time management, leadership and conflict resolution skills by taking up the responsibility of scrum master along with application design and development in the team project. I am ambitious, adventurous, assiduous & animated. Excited to pursue my dream career as web developer and ready to utilize my problem solving skills to build innovative applications.</p>
+                </div>
+
             Email Id: deepaligarg.chd@gmail.com
             <br />
             Contact Me: 404-800-3641
-        </div>
+            </div>
         </main>
     );
 }
