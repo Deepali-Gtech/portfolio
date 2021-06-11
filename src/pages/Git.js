285b11ade5fe500346126beb8d3dc4864a5b7465
Deepali Garg <deepaligarg.chd@gmail.com>
2021-06-10 23:48:37 -0400

    updated the portfolio page

--- src/pages/Git.js
+++ src/pages/Git.js
@@ -13,6 +13,8 @@ function Git() {
                     <div class="row">
                         <div class="col-4" id="fitconnect">
                             <b>Fit Connect</b>
+                            <p>Fit-Connect is a web based application that allows you to create events for fitness activities in your surrounding areas.</p>
+                            Technology - MongoDB, Express, React, Node, React-Bootstrap, Formik, Yup 
                             <li>
                                 {/*eslint-disable-next-line jsx-a11y/anchor-has-content*/}
                                 <a class="websiteLink" href="https://shrouded-shore-55489.herokuapp.com/" target="_blank" rel="noreferrer" >Deployed link</a>
@@ -36,6 +38,10 @@ function Git() {
                         </div>
                         <div class="col-4" id="outwrite">
                             <b>OutWrite</b>
+                            <p>This website is for writers looking to flex their muscles and get a little inspiration.
+
+Designed with collaboration in mind, OUTWrite is a site for writers looking to create collaborative stories. </p>
+Technology - Javascript, HTML5, Goodreads API, Random Prompt API, Bootstrap, Bulma
                             <li>
                                 {/*eslint-disable-next-line jsx-a11y/anchor-has-content*/}
                                 <a class="websiteLink" href="https://strawberry-crisp-31516.herokuapp.com/" target="_blank" rel="noreferrer" >Deployed link</a>
@@ -54,6 +60,8 @@ function Git() {
                     <div class="row">
                         <div class="col-4" id="feedme">
                             <b>Feed Me</b>
+                            <p>Worked on a website that helps you and your indecisive friends figure out where you go for dinner. </p>
+                            Technology - UIKit, Google Maps Geocoding API, Zomato API, jQuery, JavaScript, CSS, HTML5
                             <li>
                                 {/*eslint-disable-next-line jsx-a11y/anchor-has-content*/}
                                 <a class="websiteLink" href="https://a-andres1.github.io/feed-me-tm/" target="_blank" rel="noreferrer" >Deployed link</a>
@@ -77,6 +85,8 @@ function Git() {
                         </div>
                         <div class="col-4" id="feedme">
                             <b>Employee Directory</b>
+                           <p> App will aloow to view entire employee directory at once so that you have quick access to their information.</p>
+                           Technology - MongoDB, Express, React, Node, Mongoose
                             <li>
                                 {/*eslint-disable-next-line jsx-a11y/anchor-has-content*/}
                                 <a class="websiteLink" href="https://deepali-gtech.github.io/employee-directory/" target="_blank" rel="noreferrer" >Deployed link</a>
@@ -95,6 +105,8 @@ function Git() {
                     <div class="row">
                         <div class="col-4" id="googlebooksearch">
                             <b>Google Books Search</b>
+                            <p>This is a React-based Google Books Search app. This create React components, work with helper/util functions, and utilize React lifecycle methods to query and display books based on user searches.</p>
+                            Technology - MongoDB, Express, React, Node, Mongoose
                             <li>
                                 {/*eslint-disable-next-line jsx-a11y/anchor-has-content*/}
                                 <a class="websiteLink" href="https://cryptic-basin-72321.herokuapp.com/" target="_blank" rel="noreferrer" >Deployed link</a>
@@ -118,6 +130,8 @@ function Git() {
                         </div>
                         <div class="col-4" id="techblog">
                             <b>Tech Blog</b>
+                            <p>This app build a CMS-style blog site similar to a Wordpress site, where developers can publish their blog posts and comment on other developers’ posts as well.</p>
+                            Technology - MySQL, Express, Sequelize, Node, Dotenv
                             <li>
                                 {/*eslint-disable-next-line jsx-a11y/anchor-has-content*/}
                                 <a class="websiteLink" href="https://mysterious-stream-65434.herokuapp.com/" target="_blank" rel="noreferrer" >Deployed link</a>
